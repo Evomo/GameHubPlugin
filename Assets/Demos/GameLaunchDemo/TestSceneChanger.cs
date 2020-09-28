af8d97cb91e86cd4cd8a78247ac90e938f54845b
Dan Amador <danamadorpe@gmail.com>
2020-09-28 16:37:26 +0200

    migration

---
+++ Assets/Demos/GameLaunchDemo/TestSceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using MotionAI.GameHub;
+using UnityEngine;
+
+namespace Demos.GameLaunchDemo {
+	public class TestSceneChanger : MonoBehaviour {
+		public SceneReference secondScene;
+
+
+		public void LoadGame() {
+			GameHubManager.Instance.LoadGameScene(secondScene);
+		}
+
+
+		public void UnloadGame() {
+			GameHubManager.Instance.UnloadScene();
+		}
+
+	}
+}
