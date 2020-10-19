3780ec54473b344b1c33926c318ae2697b30218d
Dan Amador <danamadorpe@gmail.com>
2020-10-19 17:06:46 +0200

    started overlay

--- Assets/GamehubPlugin/Core/GameHubManager.cs
+++ Assets/GamehubPlugin/Core/GameHubManager.cs
@@ -1,52 +1,56 @@
 ﻿using System.Collections;
-using GamehubPlugin.Util;
-using MotionAI.Core;
 using MotionAI.Core.Controller;
 using MotionAI.Core.Util;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
-
 namespace GamehubPlugin.Core {
 	public class GameHubManager : Singleton<GameHubManager> {
-		private SceneReference _loadedScene;
+		private Scene _loadedScene;
 
 
-
-		private MotionAIManager CurrentManager;
+		private MotionAIManager m_CurrentManager;
 		private Session currSess;
 
-		public UnityEvent OnEndSession, OnStartSession;
+		public UnityEvent OnEndSession, OnStartSession, onGameLoad;
+
 		#region Session Handle
-
-		public void StartSession(string gameName) {
-			CurrentManager = FindObjectOfType<MotionAIManager>();
-			currSess = new Session(gameName);
-			CurrentManager.controllerManager.onMovement.AddListener(currSess.RecordMovement);
-		}
-
-
-		public void EndSession(float score = 0) {
-			if (currSess != null) {
-				currSess?.EndSession(score);
-				Debug.Log(currSess.ToString());
-				//TODO send to native sdk 
-			}
-
-			currSess = null;
-		}
 
 		#endregion
 
 		#region Scene Loading
 
-		public void LoadGameScene(SceneReference scene) {
-			if (_loadedScene == null) {
+		IEnumerator LoadGame(int sceneBuildNumber) {
+			Scene scene = SceneManager.GetSceneByBuildIndex(sceneBuildNumber);
+			AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneBuildNumber, LoadSceneMode.Additive);
+			yield return new WaitUntil(() => asyncLoad.isDone);
+
+
+			_loadedScene = SceneManager.GetSceneByBuildIndex(sceneBuildNumber);
+			SceneManager.SetActiveScene(_loadedScene);
+			onGameLoad.Invoke();
+		}
+
+
+		IEnumerator UnloadGame() {
+			AsyncOperation asyncLoad = SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+			yield return new WaitUntil(() => asyncLoad.isDone);
+			_loadedScene = SceneManager.GetSceneByBuildIndex(0);
+		}
+
+		#endregion
+
+		#region Public API
+
+		
+		public void LoadGameScene(int scene) {
+			if (_loadedScene.buildIndex == 0) {
 				StartCoroutine(LoadGame(scene));
 			}
 		}
+
 		public void UnloadScene() {
-			if (_loadedScene != null) {
+			if (_loadedScene.buildIndex == 0) {
 				StartCoroutine(UnloadGame());
 			}
 			else {
@@ -54,22 +58,29 @@ namespace GamehubPlugin.Core {
 			}
 		}
 
-		IEnumerator LoadGame(SceneReference scene) {
-			AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
-			yield return new WaitUntil(() => asyncLoad.isDone);
-
-	
-
-			SceneManager.SetActiveScene(SceneManager.GetSceneByPath(scene.ScenePath));
-			_loadedScene = scene;
-
+		public static void LoadScene(int sceneNumber) {
+			GameHubManager.Instance.LoadGameScene(sceneNumber);
 		}
 
 
-		IEnumerator UnloadGame() {
-			AsyncOperation asyncLoad = SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
-			yield return new WaitUntil(() => asyncLoad.isDone);
-			_loadedScene = null;
+		public void StartSession(string gameName) {
+			m_CurrentManager = FindObjectOfType<MotionAIManager>();
+			currSess = new Session(gameName);
+			m_CurrentManager.controllerManager.onMovement.AddListener(currSess.RecordMovement);
+		}
+
+		public void EndSession(int score = 0, int coinsCollected = 0) {
+			if (currSess != null) {
+				currSess?.EndSession(score, coinsCollected);
+				Debug.Log(currSess.ToString());
+				//TODO send to native sdk 
+			}
+
+			currSess = null;
+		}
+
+		public static void StopGame() {
+			GameHubManager.Instance.UnloadScene();
 		}
 
 		#endregion
