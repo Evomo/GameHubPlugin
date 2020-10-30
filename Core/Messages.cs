8290cf8d14767e7f0e448db6830b9b6072d6da68
Dan Amador <DanAmador@users.noreply.github.com>
2020-10-30 17:51:31 +0100

    fix: refactor and added messages

--- Core/Messages.cs
+++ Core/Messages.cs
@@ -1,15 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace GamehubPlugin.Core {
-	[Serializable]
-	public class CommunicationMessages {
-		public List<Session> sessions;
-		public CommunicationMessageType messageType;
-	}
+    [Serializable]
+    public class CommunicationMessages {
+        public List<Session> sessions;
+        public CommunicationMessageType messageType;
 
-	public enum CommunicationMessageType {
-		ENDGAME,
-		CURRENTSESSION
-	}
+        public override string ToString() {
+            if (sessions.Count == 1) {
+                return JsonUtility.ToJson(sessions.First());
+            }
+
+            return JsonUtility.ToJson(sessions);
+        }
+    }
+
+    public enum CommunicationMessageType {
+        ENDGAME,
+        CURRENTSESSION
+    }
 }
