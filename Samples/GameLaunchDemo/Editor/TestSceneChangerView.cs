4199898cc2a8caa012c51c6fd68f7e7b0d45bcdf
Dan Amador <DanAmador@users.noreply.github.com>
2020-11-03 13:08:15 +0100

    fix: added sample asmdef

--- Samples/GameLaunchDemo/Editor/TestSceneChangerView.cs
+++ Samples/GameLaunchDemo/Editor/TestSceneChangerView.cs
@@ -1,32 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using GamehubPlugin.Core;
-using GamehubPlugin.Samples.GameLaunchDemo;
+﻿using GamehubPlugin.Core;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
 
-[CustomEditor(typeof(TestSceneChanger))]
-public class TestSceneChangerview : UnityEditor.Editor {
-    public override void OnInspectorGUI() {
-        TestSceneChanger comp = (TestSceneChanger) target;
+namespace GamehubPlugin.Samples.GameLaunchDemo.Editor {
+    [CustomEditor(typeof(TestSceneChanger))]
+    public class TestSceneChangerView : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            TestSceneChanger comp = (TestSceneChanger) target;
 
 
-        if (comp.testAsset) {
-            GameHubGame game = comp.testAsset;
-            if (GUILayout.Button($"Launch {game.name}")) {
-                comp.LoadGame();
+            if (comp.testAsset) {
+                GameHubGame game = comp.testAsset;
+                if (GUILayout.Button($"Launch {game.name}")) {
+                    comp.LoadGame();
+                }
+
+                if (GUILayout.Button($"Reset {game.name}")) {
+                    comp.Restart();
+                }
+
+                if (GUILayout.Button($"Stop {game.name}")) {
+                    comp.StopGame();
+                }
             }
 
-            if (GUILayout.Button($"Reset {game.name}")) {
-                comp.Restart();
-            }
-
-            if (GUILayout.Button($"Stop {game.name}")) {
-                comp.StopGame();
-            }
+            DrawDefaultInspector();
         }
-
-        DrawDefaultInspector();
     }
 }
