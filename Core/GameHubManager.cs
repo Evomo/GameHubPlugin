7a0dd1abed2da16e85b0f9980cc92d0a3fd3b927
Jakob Wowy <jakob.wowy@evomo.de>
2020-12-08 15:30:08 +0100

    Revert "disable clear main scene"
    This reverts commit 1d1c59e6ba66c5aadaf3ac6101db708792982beb.

--- Core/GameHubManager.cs
+++ Core/GameHubManager.cs
@@ -81,28 +81,17 @@ namespace GamehubPlugin.Core {
         }
 
         IEnumerator UnloadGame() {
-            Scene activeScene = SceneManager.GetActiveScene();
-
-            Debug.Log($"GameHubManager: Get Active Scene {activeScene.name} {activeScene.buildIndex}");
-            AsyncOperation asyncLoad = SceneManager.UnloadSceneAsync(activeScene);
-            
-            Debug.Log("GameHubManager: UnloadSceneAsync started");
-            Debug.Log($"GameHubManager: {asyncLoad.isDone.ToString()}");
-                
+            AsyncOperation asyncLoad = SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
             yield return new WaitUntil(() => asyncLoad.isDone);
-            
-            Debug.Log($"GameHubManager: isDOne {asyncLoad.isDone.ToString()}");
-            
+
             isGameRunning = false;
             _hasNotifiedApp = false;
             loadedGame = null;
             m_CurrentManager = null;
-            Debug.Log("GameHubManager: Before Load Scene");
+
             _loadedScene = SceneManager.GetSceneByBuildIndex(0);
-            Debug.Log("GameHubManager: Set Main Active");
-            SceneManager.SetActiveScene(_loadedScene);
-            Debug.Log("GameHubManager: Application Unload");
             Application.Unload();
+
         }
 
         IEnumerator ResetGameCoroutine() {
@@ -168,7 +157,7 @@ namespace GamehubPlugin.Core {
             if (isGameRunning) {
                 if (_loadedScene.buildIndex > 0) {
                     _currSess = null;
-                    // CleanMainScene(); 
+                    CleanMainScene(); 
                     StartCoroutine(UnloadGame());
                 }
             }
