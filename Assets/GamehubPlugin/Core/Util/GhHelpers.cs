53b5965c0fa40bfff3d57e5d6e67fa7caf720ca3
Dan Amador <DanAmador@users.noreply.github.com>
2020-12-11 11:53:20 +0100

    fix: logs

---
+++ Assets/GamehubPlugin/Core/Util/GhHelpers.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace GamehubPlugin.Core.Util {
+    public static class GhHelpers{
+
+        public static void Log(string message) {
+            Debug.Log($"GameHub: {message}");
+        }
+    }
+}
